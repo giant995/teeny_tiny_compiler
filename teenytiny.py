3c192f6a4889cd033cd6f5c690dd314d41e8e245
jmplourde <2964972-jmplourde@users.noreply.gitlab.com>
2024-01-16 20:49:45 -0500

    Add main

---
+++ teenytiny.py
@@ -0,0 +1,14 @@
+from lex import *
+
+
+def main():
+    source = "LET foobar = 123"
+    lexer = Lexer(source)
+
+    while lexer.peek() != "\0":
+        print(lexer.curChar)
+        lexer.nextChar()
+
+
+if __name__ == "__main__":
+    main()
