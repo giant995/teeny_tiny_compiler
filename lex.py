303920edd75c870efe7bce606ee9055c59926a54
jmplourde <2964972-jmplourde@users.noreply.gitlab.com>
2024-01-17 21:15:05 -0500

    Detect basic operator tokens

--- lex.py
+++ lex.py
@@ -1,3 +1,53 @@
+import enum
+
+
+class Token:
+    """
+    Token contains the original text and the type of token.
+    """
+
+    def __init__(self, tokenText, tokenKind):
+        """
+        :param tokenText: The token's actual text. Used for identifiers, strings and numbers
+        :param tokenKind: The TokenType that this token is classified as.
+        """
+        self.text = tokenText
+        self.kind = tokenKind
+
+
+class TokenType(enum.Enum):
+    """Enum for all the types of tokens."""
+    EOF = -1
+    NEWLINE = 0
+    NUMBER = 1
+    IDENT = 2
+    STRING = 3
+    # Keywords
+    LABEL = 101
+    GOTO = 102
+    PRINT = 103
+    INPUT = 104
+    LET = 105
+    IF = 106
+    THEN = 107
+    ENDIF = 108
+    WHILE = 109
+    REPEAT = 110
+    ENDWHILE = 111
+    # Operators
+    EQ = 201
+    PLUS = 202
+    MINUS = 203
+    ASTERISK = 204
+    SLASH = 205
+    EQEQ = 206
+    NOTEQ = 207
+    LT = 208
+    LTEQ = 209
+    GT = 210
+    GTEQ = 211
+
+
 class Lexer:
     def __init__(self, source):
         """
@@ -5,13 +55,15 @@ class Lexer:
 
         :param source: Source code to lex as a string. We append a newline to simplify lexing/parsing the last
         token/statement
+        :param curChar: The current character in the string
+        :param curPos: The current position in the string
         """
         self.source = source + "\n"
-        self.curChar = ""  # Current character in the string
-        self.curPos = -1  # current position in the string
-        self.nextChar()
+        self.curChar = ""
+        self.curPos = -1
+        self.next_char()
 
-    def nextChar(self):
+    def next_char(self):
         """Process the next character."""
         self.curPos += 1
         if self.curPos >= len(self.source):
@@ -38,5 +90,26 @@ class Lexer:
         pass
 
     def getToken(self):
-        """Return the next token."""
-        pass
+        """
+        Return the next token. Check the first character of this token to see if it can decide what it is.
+        If it is a multiple character operator (e.g.: !=), number, identifier or keyword then we will process the rest.
+        """
+        token = None
+
+        if self.curChar == "+":
+            token = Token(self.curChar, TokenType.PLUS)
+        elif self.curChar == "-":
+            token = Token(self.curChar, TokenType.MINUS)
+        elif self.curChar == "*":
+            token = Token(self.curChar, TokenType.ASTERISK)
+        elif self.curChar == "/":
+            token = Token(self.curChar, TokenType.SLASH)
+        elif self.curChar == "\n":
+            token = Token(self.curChar, TokenType.NEWLINE)
+        elif self.curChar == "\0":
+            token = Token(self.curChar, TokenType.EOF)
+        else:  # unknown token
+            pass
+
+        self.next_char()
+        return token
